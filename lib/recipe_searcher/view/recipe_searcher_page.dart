2853363b0fb08322fdebc37e41f0910e17872925
LUSEDOU <dolorier_uribe@hotmail.com>
2022-07-05 21:53:56 -0500

    why I have 2 branchs?

--- lib/recipe_searcher/view/recipe_searcher_page.dart
+++ lib/recipe_searcher/view/recipe_searcher_page.dart
@@ -10,13 +10,12 @@ class RecipeSearcherPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => RecipeSearcherCubit(),
-      child: const _RecipeSearcherView(),
+      child: _RecipeSearcherView(),
     );
   }
 }
 
 class _RecipeSearcherView extends StatelessWidget {
-  const _RecipeSearcherView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,28 +25,31 @@ class _RecipeSearcherView extends StatelessWidget {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(12),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  l10n.recipeSearcherTitle,
-                  style: textTheme.headline2,
-                ),
-                const SizedBox(height: 10),
-                Text(l10n.recipeSearcherSubtitle),
-                const SizedBox(height: 10),
-                _RecipeSearcherForm(),
-              ],
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 80),
+            Text(
+              l10n.recipeSearcherTitle,
+              style: textTheme.headline2,
             ),
-          ),
-        );
-      
+            const SizedBox(height: 10),
+            Text(l10n.recipeSearcherSubtitle),
+            const SizedBox(height: 10),
+            _RecipeSearcherForm(),
+            const SizedBox(height: 10),
+            _RecipeSearcherBody(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class _RecipeSearcherForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     final _textController = TextEditingController();
 
     return TextField(
@@ -55,13 +57,64 @@ class _RecipeSearcherForm extends StatelessWidget {
       onChanged: (text) => 
         context.read<RecipeSearcherCubit>().queryChanged(text),
       decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.search_rounded),
+        filled: true,
+        labelText: l10n.recipeSearcherTextLabel,
+        prefixIcon: IconButton(
+          onPressed: () => 
+              context.read<RecipeSearcherCubit>().search(), 
+          icon: const Icon(Icons.search_rounded),
+        ),
         border: InputBorder.none,
-        suffixIcon: GestureDetector(
-          onTap: (){},
-          child: const Icon(Icons.delete),
+        suffixIcon: IconButton(
+          onPressed: () => {
+            context.read<RecipeSearcherCubit>().queryChanged(''), 
+            _textController.clear(),
+          },
+          icon: const Icon(Icons.delete_sweep_rounded),
         ),
       ),
     );
+  }
+}
+
+class _RecipeSearcherBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return BlocBuilder<RecipeSearcherCubit, RecipeSearcherState>(
+      buildWhen: (previous, current) => 
+          previous.status != current.status,
+      builder: (context, state) {
+        final status = state.status;
+
+        return status == RecipeSearcherStatus.success
+            ? _RecipeList()
+            : Center(
+              child: status == RecipeSearcherStatus.loading
+                  ? const CircularProgressIndicator()
+                  : Column(
+                    children: <Widget>[
+                      const Icon(
+                        Icons.food_bank,
+                        size: 150,
+                      ),
+                      const SizedBox(height: 8),
+                      Text( status == RecipeSearcherStatus.initial
+                          ? l10n.recipeListInitial
+                          : l10n.recipeListNotFoundQuery(state.query),
+                      ),
+                    ],
+                  ),
+            );
+      },
+    );
+  }
+}
+
+class _RecipeList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
